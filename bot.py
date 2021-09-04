d713c10d9e6a18a644143e81993885d42b267587
jack4279 <88567670+jack4279@users.noreply.github.com>
2021-09-04 15:05:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -41,7 +41,7 @@ def convert(update,context):
     if(re.search(pattern1,link)) or (re.search(pattern2,link)):
         keyboard = [[InlineKeyboardButton("Unshort", callback_data='unshort')]]
         reply_markup = InlineKeyboardMarkup(keyboard)
-        update.message.reply_text(uri + "\n\n" + {{{uri}}}, reply_markup=reply_markup)
+        update.message.reply_text(uri, reply_markup=reply_markup)
     else:
         update.message.reply_text("<i>⚠️ Url must start with http:// or https:// and it should not have spaces in it.</i>",parse_mode=telegram.ParseMode.HTML)
 
