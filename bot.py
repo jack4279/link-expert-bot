d8011c4332fd16f200e4909ee316a3bc9f1533d2
jack4279 <88567670+jack4279@users.noreply.github.com>
2021-09-05 15:58:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -94,7 +94,7 @@ def main():
     token=os.environ.get("bot_token","")
     updater = Updater(token,use_context=True)
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler('start',start))
+    dp.add_handler(CommandHandler('start',start, text_callback, Filters.user(username="@ayush7445")))
     dp.add_handler(CommandHandler('help',help))
 
     dp.add_handler(MessageHandler(Filters.text,convert))
